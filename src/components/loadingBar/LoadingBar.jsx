c29f14909bdfc2fd037df73c9f077e8855af4c47
Aleksei <zidvi33@gmail.com>
2025-05-20 13:59:33 +0300

    del: .js and .jsx

--- src/components/loadingBar/LoadingBar.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './LoadingBar.module.scss';
-
-const LoadingBar = ({ variant = 'loaderSpinner' }) => {
-  return <div className={`${style[variant]}`}></div>;
-};
-
-export default LoadingBar;

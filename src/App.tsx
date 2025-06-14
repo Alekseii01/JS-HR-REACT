b314eca1e518a0c6a0fe6bea0d9b72d11652a26f
Aleksei <zidvi33@gmail.com>
2025-06-14 18:50:41 +0300

    Complete HT-9

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import Footer from "./components/footer/Footer";
 import HomePage from "./pages/homePage/HomePage";
 import MenuPage from "./pages/menuPage/MenuPage";
 import LogInPage from "./pages/logInPage/LogInPage";
+import OrderPage from "./pages/orderPage/OrderPage";
+import Page404 from "./pages/404Page/Page404";
 import ProtectedRoute from "./routes/ProtectedRoute";
 
 const App: React.FC = () => {
@@ -33,6 +35,8 @@ const App: React.FC = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LogInPage />} />
         <Route path="/menu" element={<ProtectedRoute element={<MenuPage />} />} />
+        <Route path="/cart" element={<ProtectedRoute element={<OrderPage />} />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
       <Footer />
     </>
